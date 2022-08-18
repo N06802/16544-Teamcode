518c5952a47eeb1f3d29744d4ad5c26477ca3340
N06802 <starwarsfan12356@gmail.com>
2022-08-17 19:59:38 -0600

    Create HardwareMap.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class HardwareMap {
+
+    //Create motors
+    public DcMotor leftFront  = null;
+    public DcMotor rightFront = null;
+    
+}
